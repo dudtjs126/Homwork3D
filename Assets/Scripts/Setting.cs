61155ae8f0b4473fbfa111d5ad2b14940da5617c
dudtjs126 <dudtjs126@gmail.com>
2024-03-04 20:02:31 +0900

    Initial commit

---
+++ Assets/Scripts/Setting.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setting : MonoBehaviour
+{
+    public GameObject settingBack;
+
+    public void Settings()
+    {
+        settingBack.SetActive(true);
+    }
+}
